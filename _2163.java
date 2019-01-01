dda74e1d1dc27825784f153b65be5760ed2343a5
Triple_J <apjojeongjin@gmail.com>
2019-01-02 08:51:53 +0900

    초콜릿 자르기 풀이 완료

---
+++ _2163.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class _2163 {
+	public static void main(String[] a) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print(scan.nextInt()*scan.nextInt()-1);
+	}
+}
