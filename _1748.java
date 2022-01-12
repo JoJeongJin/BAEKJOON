394d84352b3c68b87e0201c1f781d8faffa3e8ee
JoJeonogJin <apjojeongjin@gmail.com>
2022-01-12 15:50:08 +0900

    더 빠른 구조

--- _1748.java
+++ _1748.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class _1748 {
+import java.util.Scanner;
+
+public class _1748 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+
+        int length = 1;
+        int answer = 0;
+        int temp = 10;
+
+        for(int i=1; i<=input; i++){
+            if(temp == i){
+                length++;
+                temp*=10;
+            }
+            answer+=length;
+        }
+        System.out.println(answer);
+    }
+
+
 }
