1e3b11805777914034242ced3307cb103c4de942
JoJeonogJin <apjojeongjin@gmail.com>
2022-01-12 16:03:36 +0900

    분수 찾기

--- _1193.java
+++ _1193.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class _1193 {
+import java.util.Scanner;
+
+public class _1193 {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 1;
+
+        boolean isYDirection = true;
+
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+
+        for(int i=2; i<=input; i++){
+            if(isYDirection && x==1){
+                y++;
+                isYDirection = false;
+                continue;
+            }
+            if(!isYDirection && y==1){
+                x++;
+                isYDirection = true;
+                continue;
+            }
+
+            if(isYDirection){
+                y++;
+                x--;
+            }else{
+                x++;
+                y--;
+            }
+        }
+        System.out.println(x+"/"+y);
+    }
 }
