35e0506a71d831fcb954da9c7c0789448a0ad39c
JoJeonogJin <apjojeongjin@gmail.com>
2023-03-15 23:57:41 +0900

    1

---
+++ _4375.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _4375 {
+    //1로만 이루어진 n의 배수?
+    //
+    public static void main(String[] args) throws IOException {
+        String input;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while ((input = br.readLine()) != null) {
+            int inputChangeAsInteger = Integer.parseInt(input);
+
+            int start = 1;
+            int length = 1;
+            while (true) {
+                start %= inputChangeAsInteger;
+                if (start % inputChangeAsInteger == 0) {
+                    System.out.println(length);
+                    break;
+                }
+                start *= 10;
+                start += 1;
+                length++;
+            }
+        }
+    }
+}
