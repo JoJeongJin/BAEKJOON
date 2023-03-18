c6125f575d0b5a5901a01920321bdc2d364e5689
JoJeonogJin <apjojeongjin@gmail.com>
2023-03-18 16:24:13 +0900

    백준 문제 풀고 코드 제출할 때 사용하는 Template 업데이트

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args){
-        solve();
+    public static void main(String[] args) throws Exception {
+        new Main().solve();
     }
 
     /**
     아래 부분에 코드 작성
      */
-    private static void solve(){
-        //입력
-        
-        //알고리즘 처리
-        
-        //출력
+    public void solve() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //이후 코드 작성
     }
 }
