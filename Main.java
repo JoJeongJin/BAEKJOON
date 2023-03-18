b049b134156854e4b64db0e171020528aecda7a7
JoJeonogJin <apjojeongjin@gmail.com>
2023-03-18 16:20:30 +0900

    백준 문제 풀고 코드 제출할 때 사용하는 Template 작성

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        solve();
+    }
+
+    /**
+    아래 부분에 코드 작성
+     */
+    private static void solve(){
+        //입력
+        
+        //알고리즘 처리
+        
+        //출력
+    }
+}
